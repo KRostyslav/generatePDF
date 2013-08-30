5a6fc2b861da117eb3c4edd8ead547080199e621
Rostyslav <rostyslav.kryvyy@gmail.com>
2013-08-30 10:54:28 +0300

    add password protection to PDF

--- generatePDF/src/generatePDF/createPDF.java
+++ generatePDF/src/generatePDF/createPDF.java
@@ -11,30 +11,35 @@ import com.lowagie.text.pdf.PdfWriter;
 
 public class createPDF {
 
+	static String USER_PASS = "user";
+
+	static String OWNER_PASS = "owner";
+
 	public static void main(String[] args) {
-		 try {
-	            OutputStream file = new FileOutputStream(new File("file.pdf"));
-	 
-	            Document document = new Document();
-	            PdfWriter.getInstance(document, file);
-	 
-	            document.open();
-	            document.add(new Paragraph("First PDF-file"));
-	            document.add(new Paragraph(new Date().toString()));
-	            
-	            
-	            document.addAuthor("Ros KRostyslav");
-	            document.addCreationDate();
-	            document.addCreator("iText library");
-	            document.addTitle("Title of PDF file");
-	 
-	            document.close();
-	            file.close();
-	 
-	        } catch (Exception e) {
-	 
-	            e.printStackTrace();
-	        }
+		
+		try {
+			OutputStream file = new FileOutputStream(new File("file.pdf"));
+			Document document = new Document();			
+			PdfWriter writer = PdfWriter.getInstance(document, file);
+			writer.setEncryption(USER_PASS.getBytes(), OWNER_PASS.getBytes(),
+					PdfWriter.ALLOW_PRINTING, PdfWriter.ENCRYPTION_AES_128);
+
+			document.open();			
+			document.addAuthor("Ros KRostyslav");
+			document.addCreationDate();
+			document.addCreator("iText library");
+			document.addTitle("Title of PDF file");			
+			document.add(new Paragraph("First PDF-file"));
+			document.add(new Paragraph(new Date().toString()));
+			document.close();
+			file.close();
+			
+			System.out.println("PDF generated!!!");
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
 
 	}
 
