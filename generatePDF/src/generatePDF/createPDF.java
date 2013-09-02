64977e2169f3b1dfacd40012eae479b1fff5e108
Rostyslav <rostyslav.kryvyy@gmail.com>
2013-09-02 21:39:03 +0300

    update code add methods and interface

--- generatePDF/src/generatePDF/createPDF.java
+++ generatePDF/src/generatePDF/createPDF.java
@@ -6,41 +6,48 @@ import java.io.OutputStream;
 import java.util.Date;
 
 import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
 
-public class createPDF {
-
-	static String USER_PASS = "user";
-
-	static String OWNER_PASS = "owner";
+public class createPDF implements dateForPDF{
 
 	public static void main(String[] args) {
-		
+
 		try {
 			OutputStream file = new FileOutputStream(new File("file.pdf"));
-			Document document = new Document();			
-			PdfWriter writer = PdfWriter.getInstance(document, file);
-			writer.setEncryption(USER_PASS.getBytes(), OWNER_PASS.getBytes(),
-					PdfWriter.ALLOW_PRINTING, PdfWriter.ENCRYPTION_AES_128);
-
-			document.open();			
-			document.addAuthor("Ros KRostyslav");
-			document.addCreationDate();
-			document.addCreator("iText library");
-			document.addTitle("Title of PDF file");			
-			document.add(new Paragraph("First PDF-file"));
-			document.add(new Paragraph(new Date().toString()));
+			Document document = new Document();
+			setEncryptionPDF(document, file);
+			document.open();
+			setAtributePDF(document);
+			inputTextPDF(document);
 			document.close();
 			file.close();
-			
 			System.out.println("PDF generated!!!");
-
 		} catch (Exception e) {
-
 			e.printStackTrace();
 		}
-
 	}
 
+	public static void setEncryptionPDF(Document document, OutputStream file) throws DocumentException {
+		PdfWriter writer = PdfWriter.getInstance(document, file);
+		writer.setEncryption(USER_PASS.getBytes(), OWNER_PASS.getBytes(),
+				PdfWriter.ALLOW_PRINTING, PdfWriter.ENCRYPTION_AES_128);
+		System.out.println("password..");
+	}
+
+	public static void setAtributePDF(Document document) {
+		document.addAuthor(AUTHOR);
+		document.addCreationDate();
+		document.addCreator(CREATOR);
+		document.addTitle(TITLE);
+		System.out.println("Atribute...");
+	}
+
+	public static void inputTextPDF(Document document) throws DocumentException {
+		document.add(new Paragraph("First PDF-file"));
+		document.add(new Paragraph(new Date().toString()));
+		System.out.println("text...");
+	}
+	
 }
---
+++ generatePDF/src/generatePDF/dateForPDF.java
@@ -0,0 +1,9 @@
+package generatePDF;
+
+public interface dateForPDF {
+	static String USER_PASS = "user";
+	static String OWNER_PASS = "owner";
+	static String AUTHOR = "Rostyslav";
+	static String CREATOR = "iText library";
+	static String TITLE = "PDF file. Book. ";
+}
